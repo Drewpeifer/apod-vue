d82202259ecf3247bd62a49b7d2b847c500e8100
drewpeifer <peiftronica@gmail.com>
2022-03-02 14:12:37 -0500

    created dummy post for date out of range

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -10,7 +10,7 @@
 		}}">
 		<div>
 			<h1>{{post.title}}</h1>
-			<p class="text-xs">{{post.date}}</p>
+			<p class="text-xs mb-2">{{ niceDate(post.date) }}</p>
 			<img class="mx-auto max-w-xl" :src="post.url" />
 			<p v-if="post.copyright" class="text-xs">Copyright: {{post.copyright}}</p>
 			<p v-else class="text-xs">Copyright: NASA</p>
@@ -21,6 +21,27 @@
 
 <script>
 export default {
-	props: ['post']
+	data: (post) => ({
+		date: 'post.date'
+	}),
+	props: ['post'],
+	methods: {
+		niceDate(date) {
+			let arr = date.split('-'),
+				y = arr[0],
+				m = arr[1],
+				d = arr[2],
+				niceDate = m + '/' + d + '/' + y;
+
+				if (date === 'undefined') {
+					// when user asks for a date outside the API's range, we feed
+					// a static string "undefined" to the obj, which should not be parsed,
+					// and is replaced by an empty string for the UI
+					niceDate = "(Search must fall between Jun 16, 1995 and Mar 02, 2022)";
+				}
+
+			return niceDate;
+		}
+	}
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
-<div class="flex mt-5 flex-row justify-center w-min mx-auto">
-	<input type="date" placeholder="MM/DD/YYYY" class="text-right p-2 mr-2 border-blue-500 border-2 rounded-full" v-model="searchDate" />
-	<button @click="search" class="searchBtn bg-blue-900 hover:bg-blue-600 text-white font-bold px-4 rounded-full">Search</button>
-</div>
-<div class="flex grow mt-5 flex-row justify-center mx-auto ">
-	<button @click="randomSearch" class="searchBtn bg-green-900 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full w-min whitespace-nowrap">Surprise me!</button>
-</div>
-<div class="px-5 grow flex flex-wrap justify-center flex-row">
-	<Card v-for="post in posts" :post="post" :key="post.date" />
-</div>
-<div class="footer self-end text-center pt-5 text-xs">
-	<p>Powered by VueJS and NASA</p>
-</div>
+	<div class="flex mt-5 flex-row justify-center w-min mx-auto">
+		<input type="date" placeholder="MM/DD/YYYY" class="text-right p-2 mr-2 border-blue-500 border-2 rounded-full" v-model="searchDate" />
+		<button @click="search" class="bg-blue-900 hover:bg-blue-600 text-white font-bold px-4 rounded-full">Search</button>
+	</div>
+	<div class="flex grow mt-5 flex-row justify-center mx-auto ">
+		<button @click="randomSearch" class="bg-green-900 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full w-min whitespace-nowrap">Surprise me!</button>
+	</div>
+	<div class="px-5 grow flex flex-wrap justify-center flex-row">
+		<Card v-for="post in posts" :post="post" :key="post.date" />
+	</div>
+	<div class="footer self-end text-center pt-5 text-white text-xs">
+		<p>Powered by 
+			<a class="underline hover:text-blue-800 visited:text-purple-600" href="https://vuejs.org/" target="_blank">Vue.js</a> and 
+			<a class="underline hover:text-blue-800 visited:text-purple-600" href="https://github.com/nasa/apod-api" target="_blank">NASA</a></p>
+	</div>
 </template>
 
 <script>
@@ -32,7 +34,7 @@ export default {
 
 		// gets the date from the input, or if the input is empty, returns current date
 		function refreshDate() {
-
+			console.log('refreshing date!');
 			if (!state.searchDate) {
 				// input empty, return today's date
 				let today = new Date();
@@ -46,10 +48,10 @@ export default {
 		// sanitizes a date string
 		function sanitizeDate(date) {
 			var dd = String(date.getDate()).padStart(2, '0'),
-				mm = String(date.getMonth() + 1).padStart(2, '0'),
-				yyyy = date.getFullYear(),
-				uglyDate = yyyy + "-" + mm + "-" + dd,
-				niceDate = mm + '/' + dd + '/' + yyyy;
+			mm = String(date.getMonth() + 1).padStart(2, '0'),
+			yyyy = date.getFullYear(),
+			uglyDate = yyyy + "-" + mm + "-" + dd,
+			niceDate = mm + '/' + dd + '/' + yyyy;
 
 			state.searchDate = uglyDate;
 			return niceDate;
@@ -60,6 +62,18 @@ export default {
 			fetch("https://api.nasa.gov/planetary/apod?api_key=up0wVqmYq3dlP8SAGdRVWljjiELKm44P38DRGuNC&date=" + state.searchDate)
 			.then((res) => res.json())
 			.then((data) => {
+				if (data.code) {
+					// invalid date / no data, so build a dummy post
+					data = {
+						date: "undefined",// passed to niceDate in Card.vue
+						explanation: "",
+						media_type: "image",
+						service_version: "v1",
+						title: "Sorry, no picture for that date :(",
+						url: "/einstein.jpeg",
+						copyright: "None"
+					}
+				}
 				console.log(data);
 				state.posts = [data];
 				console.dir(state);
@@ -72,7 +86,7 @@ export default {
 			.then((res) => res.json())
 			.then((data) => {
 				console.log(data);
-					state.posts = data;
+				state.posts = data;
 				console.dir(state);
 			})
 		}
