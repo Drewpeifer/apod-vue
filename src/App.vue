ff2d132c7635152bdab18adbc6304b57f6fc595a
drewpeifer <peiftronica@gmail.com>
2022-03-04 02:08:13 -0500

    lighter bg for body and centered header text

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="p-5 bg-gray-900">
-    <router-link class="flex flex-col justify-center text-white font-mono font-Nasa" to="/">
+    <router-link class="flex flex-col justify-center text-center text-white font-mono font-Nasa" to="/">
       <span class='mx-auto w-content text-5xl'>NASA</span>
       <span class='mx-auto w-content text-3xl'>ASTRONOMY PICTURE OF THE DAY</span>
     </router-link>
